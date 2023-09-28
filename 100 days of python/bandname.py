a16f4fe7409f6a32c3b86d7697c07888a2f74552
Davikking <53028244+Davikking@users.noreply.github.com>
2023-09-28 14:31:44 -0400

    lots of changes
    working on personal website and the dom model as well as starting 100 days of python code challenge

---
+++ 100 days of python/bandname.py
@@ -0,0 +1,4 @@
+print("welcome to the band name generator")
+city = input("what is the name of the city your grew up in?")
+pet = input("what is your pets name?")
+print("your band name could be " +city+" "+pet )
